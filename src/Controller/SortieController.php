13b599a10584b50ac774700d8ee512d7c4c1ae59
JulieLuss <julie.lusseau2021@campus-eni.fr>
2022-02-23 10:19:37 +0100

    Changement de nom Controller

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -17,7 +17,7 @@ class SortieController extends AbstractController
     /**
      * @Route("/sortie", name="sortie_afficher")
      */
-    public function afficher(SortieRepository $sortieRepository): Response
+    public function afficherTableau(SortieRepository $sortieRepository): Response
     {
         $sorties=$sortieRepository->findAll();
         return $this->render('sortie/afficher.html.twig', [
