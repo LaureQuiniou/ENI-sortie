0dcb014182a1f825feba87e898408312d7d34cd9
laurequiniou <laure.quiniou2021@campus-eni.fr>
2022-02-25 15:44:51 +0100

    navbar et debut annulerSortie

--- src/Controller/AnnulerSortieController.php
+++ src/Controller/AnnulerSortieController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Repository\SortieRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnnulerSortieController extends AbstractController
 {
 
     /**
-     * @Route ("/annulerSortie", name="annuler_sortie")
+     * @Route ("/annulerSortie/{id}", name="annuler_sortie", methods={"GET"})
      */
-    public function annulerSortie()
+    public function annulerSortie(int $id, SortieRepository $sortieRepository): Response
     {
-        return $this->render('annuler_sortie/annulerSortie.html.twig');
+        $sorties=$sortieRepository->find($id);
+
+        return $this->render('annuler_sortie/annulerSortie.html.twig', [
+            "sorties"=>$sorties
+
+        ]);
     }
 }
