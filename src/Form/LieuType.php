625a2beb1f5f576c073a630b9b4f92986311d2a2
dupuy <aurelie.dupuy2021@campus-eni.fr>
2022-03-02 09:23:29 +0100

    nouvelle sortie début lieu

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -4,12 +4,15 @@ namespace App\Form;
 
 use App\Entity\Lieu;
 use App\Entity\Ville;
+use App\Repository\LieuRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LieuType extends AbstractType
@@ -18,13 +21,39 @@ class LieuType extends AbstractType
     {
         $builder
             ->add('ville',TextType::class,['attr'=> ['list' => 'villes' ]])
-            ->add('nom')
+            ->add('nom', ChoiceType::class,['label'=>'Lieu'])
             ->add('rue')
             ->add('latitude')
             ->add('longitude')
 
         ;
+        /*$builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event)  {
+            if (null !== $event->getData()->getVille()) {
+                // we don't need to add the friend field because
+                // the message will be addressed to a fixed friend
+                return;
+            }
+
+            $form = $event->getForm();
+
+            $formOptions = [
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+                'query_builder' => function (LieuRepository $lieuRepository) use ($ville) {
+                    // call a method on your repository that returns the query builder
+                    // return $userRepository->createFriendsQueryBuilder($user);
+                },
+            ];
+
+            // create the field, this is similar the $builder->add()
+            // field name, field type, field options
+            $form->add('friend', EntityType::class, $formOptions);
+        });*/
     }
+
+    // ...
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
